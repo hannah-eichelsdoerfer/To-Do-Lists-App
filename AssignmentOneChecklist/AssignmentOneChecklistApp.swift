04a345df9d9132b2818891a916f1845dc86d6b41
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-03-21 15:47:58 +1000

    Initial Commit

---
+++ AssignmentOneChecklist/AssignmentOneChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentOneChecklistApp.swift
+//  AssignmentOneChecklist
+//
+//  Created by Hannah on 21/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentOneChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentOneChecklistUITests/AssignmentOneChecklistUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AssignmentOneChecklistUITestsLaunchTests.swift
+//  AssignmentOneChecklistUITests
+//
+//  Created by Hannah on 21/3/2023.
+//
+
+import XCTest
+
+final class AssignmentOneChecklistUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
