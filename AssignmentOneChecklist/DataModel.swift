6220d9059fd700b0e547c717a7f239410d040926
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-03-24 15:38:09 +1000

    add a Category struct to the data model file with the properties id, name and color

--- AssignmentOneChecklist/DataModel.swift
+++ AssignmentOneChecklist/DataModel.swift
@@ -6,11 +6,19 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct TaskModel: Identifiable {
     let id: UUID = UUID()
     let text: String
+    let categoryId: Int
     var checked: Bool = false
+}
+
+struct Category {
+    let id: Int
+    let name: String
+    let color: Color
 }
 
 
