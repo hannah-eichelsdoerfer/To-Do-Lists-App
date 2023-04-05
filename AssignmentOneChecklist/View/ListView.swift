7fb6d67ab3d2de2e1170f0556325c45401207280
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-04-05 15:06:03 +1000

    add textfield and functionality to add new task to list

--- AssignmentOneChecklist/View/ListView.swift
+++ AssignmentOneChecklist/View/ListView.swift
@@ -11,17 +11,30 @@ struct ListView: View {
     var checklist: Checklist
 
     @State var tasks: [Task] = testTasks
+    @State var newTask : String = ""
     
     var body: some View {
         VStack {
             Text(checklist.name).font(.title)
             List {
+                HStack {
+                    TextField("Enter new task", text: self.$newTask)
+                    Button(action: addNewTask, label: { Image(systemName: "plus").resizable()
+                            .padding(6)
+                            .frame(width: 24, height: 24)
+                            .background(Color.blue)
+                            .clipShape(Circle())
+                            .foregroundColor(.white) })
+                }
                 ForEach(tasks) { task in
                     TaskRowView(task: task)
                 }
             }
             .listStyle(.plain)
-            .navigationTitle("To-Do List")
         }
+    }
+    
+    func addNewTask() {
+        tasks.append(Task(text: newTask))
     }
 }
