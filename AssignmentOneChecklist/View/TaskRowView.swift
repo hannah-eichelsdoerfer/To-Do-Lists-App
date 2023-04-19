c15b3055cb313fbebaf4ca4cc8431d108122db13
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-04-19 20:31:16 +1000

    add padding to Task Text incase of long task.name

--- AssignmentOneChecklist/View/TaskRowView.swift
+++ AssignmentOneChecklist/View/TaskRowView.swift
@@ -5,15 +5,19 @@
 //  Created by Hannah on 24/3/2023.
 //
 import SwiftUI
+import ConfettiSwiftUI
 
 struct TaskRowView: View {
     var task: Task
 
     var body: some View {
         return HStack {
-            Text(task.text)
+            Text(task.text).padding([.trailing], 30)
             Spacer()
-            Image(systemName: task.checked ? "checkmark.square.fill" : "square").resizable().foregroundColor(task.checked ? .black : .gray).frame(width: 24, height: 24)
+            Image(systemName: task.checked ? "checkmark.square.fill" : "square")
+                .resizable()
+                .foregroundColor(task.checked ? .accentColor : .gray)
+                .frame(width: 24, height: 24)
         }
         .padding([.vertical], 10)
         .listRowSeparator(.hidden)
