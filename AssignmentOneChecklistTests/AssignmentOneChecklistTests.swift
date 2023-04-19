b72c82088c9d82371af524c1ccfacfbd1d74fd7b
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-04-19 23:45:17 +1000

    add tests for data model

--- AssignmentOneChecklistTests/AssignmentOneChecklistTests.swift
+++ AssignmentOneChecklistTests/AssignmentOneChecklistTests.swift
@@ -18,18 +18,7 @@ final class AssignmentOneChecklistTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testInitialization() {
-        let expectedLists = testLists
-        var dataModel = DataModel()
-        dataModel.lists = expectedLists
-        dataModel.save()
-        
-        let newDataModel = DataModel()
-        XCTAssertEqual(newDataModel.lists.count, expectedLists.count)
-        XCTAssertEqual(newDataModel.lists.first?.name, expectedLists.first?.name)
-        XCTAssertEqual(newDataModel.lists.first?.tasks.count, expectedLists.first?.tasks.count)
-    }
-    
+    /// This function tests the ``DataModel/load()`` function.
     func testLoadData() {
         let expectedLists = [
             Checklist(name: "Shopping", tasks: [
@@ -54,16 +43,74 @@ final class AssignmentOneChecklistTests: XCTestCase {
         XCTAssertEqual(loadedDataModel.lists.first?.tasks.count, expectedLists.first?.tasks.count)
     }
     
+    /// This function tests the ``DataModel/save()`` function.
     func testSaveData() {
-        let expectedLists = testLists
         var dataModel = DataModel()
-        dataModel.lists = expectedLists
+        dataModel.lists = testLists
         dataModel.save()
         
         let loadedDataModel = DataModel()
-        XCTAssertEqual(loadedDataModel.lists.count, expectedLists.count)
-        XCTAssertEqual(loadedDataModel.lists.first?.name, expectedLists.first?.name)
-        XCTAssertEqual(loadedDataModel.lists.first?.tasks.count, expectedLists.first?.tasks.count)
+        XCTAssertEqual(loadedDataModel.lists.count, testLists.count)
+        XCTAssertEqual(loadedDataModel.lists.first?.name, testLists.first?.name)
+        XCTAssertEqual(loadedDataModel.lists.first?.tasks.count, testLists.first?.tasks.count)
+    }
+
+    /// This function tests the ``Checklist/addNewChecklist()`` function.
+    func testAddNewChecklist() {
+        var dataModel = DataModel()
+        dataModel.lists = testLists
+        dataModel.addNewChecklist()
+        
+        XCTAssertEqual(dataModel.lists.count, 4)
+        XCTAssertEqual(dataModel.lists.last?.name, "New Checklist")
+        XCTAssertEqual(dataModel.lists.last?.icon, .checklist)
+    }
+
+    /// This function tests the ``Checklist/delete(at: offset)`` function.
+    func testDeleteChecklist() {
+        var dataModel = DataModel()
+        dataModel.lists = testLists
+        dataModel.delete(at: IndexSet(integer: 0))
+        
+        XCTAssertEqual(dataModel.lists.count, 2)
+        XCTAssertEqual(dataModel.lists.first?.name, "Uni")
+    }
+    
+    /// This function tests the ``Checklist/reset()`` and ``Checklist/undo()`` functions.
+    func testResetAndUndo() {
+        var dataModel = DataModel()
+        dataModel.lists = testLists
+
+        dataModel.lists[0].reset()
+    
+        XCTAssertEqual(dataModel.lists[0].tasks[0].checked, false)
+        XCTAssertEqual(dataModel.lists[0].tasks[1].checked, false)
+        XCTAssertEqual(dataModel.lists[0].tasks[2].checked, false)
+        
+        dataModel.lists[0].undo()
+        
+         XCTAssertEqual(dataModel.lists[0].tasks[0].checked, false)
+         XCTAssertEqual(dataModel.lists[0].tasks[1].checked, true)
+         XCTAssertEqual(dataModel.lists[0].tasks[2].checked, true)
+    }
+
+    /// This function tests if the ``Checklist/Icon`` enum is working correctly.
+    func testIconMatching() {
+        let expectedLists = [
+            Checklist(name: "Shopping", tasks: [
+                Task(text: "Milk"),
+                Task(text: "Bread"),
+                Task(text: "Eggs"),
+            ], icon: .cart),
+            Checklist(name: "Gym", tasks: [
+                Task(text: "Pushups", checked: true),
+                Task(text: "Squats"),
+                Task(text: "Bench Press"),
+            ], icon: .gamecontroller),
+        ]
+        
+        XCTAssertEqual(expectedLists.first?.icon, .cart)
+        XCTAssertEqual(expectedLists.last?.icon, .gamecontroller)
     }
 
     func testPerformanceExample() throws {
